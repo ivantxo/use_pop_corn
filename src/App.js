5fc8aada757ccf9cb08b9a58ea86b7af6c7c3566
IvanR <ivan.ramirezf@gmail.com>
2023-12-01 04:40:56 +1000

    Added useEffect to MovieDetails
    - Added useEffect to load MovieDetails into MovieDetails component

--- src/App.js
+++ src/App.js
@@ -149,6 +149,7 @@ export default function App() {
             <MovieDetails
               selectedId={selectedId}
               onCloseMovie={handleCloseMovie}
+              apiKey={KEY}
             />
           ) : (
             <>
--- src/WatchedBox/MovieDetails.js
+++ src/WatchedBox/MovieDetails.js
@@ -1,10 +1,66 @@
-function MovieDetails({ selectedId, onCloseMovie }) {
+import { useEffect, useState } from "react";
+import StarRating from "../StarRating";
+
+function MovieDetails({ selectedId, onCloseMovie, apiKey }) {
+  const [movie, setMovie] = useState({});
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        const res = await fetch(
+          `http://www.omdbapi.com/?apiKey=${apiKey}&i=${selectedId}`
+        );
+        const data = await res.json();
+        setMovie(data);
+      }
+      getMovieDetails();
+    },
+    [selectedId]
+  );
+
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
-      {selectedId}
+      <header>
+        <button className="btn-back" onClick={onCloseMovie}>
+          &larr;
+        </button>
+        <img src={poster} alt={`Poster of ${movie} movie`} />
+        <div className="details-overview">
+          <h2>{title}</h2>
+          <p>
+            {released} &bull; {runtime}
+          </p>
+          <p>{genre}</p>
+          <p>
+            <span>⭐</span>
+            {imdbRating} IMDb rating
+          </p>
+        </div>
+      </header>
+
+      <section>
+        <div className="rating">
+          <StarRating maxRating={10} size={24} />
+        </div>
+        <p>
+          <em>{plot}</em>
+        </p>
+        <p>Starring: {actors}</p>
+        <p>Directed by {director}</p>
+      </section>
     </div>
   );
 }
